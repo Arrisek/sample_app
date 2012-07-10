8730590d82b5f7c310551fc3ccadb22aecdc86e2
Kamil Maslowski <arrisprl@gmail.com>
2012-07-10 11:07:57 +0200

    Add email downcase test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -63,6 +63,16 @@ describe User do
     	end
     end
 
+    describe "email address with mixed case" do
+        let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+        it "should be saved as all lower-case" do
+            @user.email = mixed_case_email
+            @user.save
+            @user.reload.email.should == mixed_case_email.downcase
+        end
+    end
+
     describe "when email is already taken" do 
     	before do
     		user_with_same_email = @user.dup
@@ -86,6 +96,11 @@ describe User do
 	describe "when password confirmation is nil" do
        before { @user.password_confirmation = nil }
        it { should_not be_valid }
+    end
+
+    describe "with a password that's too short" do
+ 	   before { @user.password = @user.password_confirmation = "a" * 5 }
+       it { should be_invalid }
     end
 
     describe "return value of authenticate method" do
